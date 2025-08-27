a94e0a615c7c2ff0d117280b296babfe72c073b5
nithu <nithupm8@gmail.com>
2025-08-27 23:18:42 +0530

    barchart

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -1,14 +1,14 @@
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectDate, clearSelection } from "../store/calendarSlice";
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import { selectDate } from "../store/calendarSlice";
+import BarChartPopup from "./BarChartPopup";
 
 const localizer = momentLocalizer(moment);
 
-export default function CalendarView() {
+const CalendarView = () => {
   const dispatch = useDispatch();
   const { data, selectedDate, selectedData, alert } = useSelector((state) => state.calendar);
 
@@ -44,46 +44,14 @@ export default function CalendarView() {
 
       {alert && (
         <div style={{ marginTop: "20px", padding: "10px", background: "#fff3cd", color: "#856404", border: "1px solid #ffeeba" }}>
-         {alert}
+        {alert}
         </div>
       )}
 
-      {selectedData && (
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background: "rgba(0,0,0,0.5)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <div style={{ background: "white", padding: "20px", borderRadius: "8px", width: "600px" }}>
-            <h3>Data for {selectedDate}</h3>
-
-            <ResponsiveContainer width="100%" height={300}>
-              <BarChart data={selectedData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="user" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="value" fill="#8884d8" />
-              </BarChart>
-            </ResponsiveContainer>
-
-            <button
-              onClick={() => dispatch(clearSelection())}
-              style={{ marginTop: "15px", padding: "8px 16px", background: "#007bff", color: "white", border: "none", borderRadius: "4px", cursor: "pointer" }}
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      
+      {selectedData && <BarChartPopup date={selectedDate} data={selectedData} />}
     </div>
   );
 }
+
+export default CalendarView;
