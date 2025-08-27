748dbaabc7edc3f8bf2aa9a293058b76863f5884
nithu <nithupm8@gmail.com>
2025-08-27 22:55:23 +0530

    bar chart implemented

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -3,15 +3,15 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useDispatch, useSelector } from "react-redux";
-import { selectDate } from "../store/calendarSlice";
+import { selectDate, clearSelection } from "../store/calendarSlice";
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 
 const localizer = momentLocalizer(moment);
 
 export default function CalendarView() {
   const dispatch = useDispatch();
-  const { data } = useSelector((state) => state.calendar);
+  const { data, selectedDate, selectedData, alert } = useSelector((state) => state.calendar);
 
- 
   const events = useMemo(() => {
     return Object.keys(data).map((dateStr) => {
       const date = moment(dateStr, "DD-MM-YYYY").toDate();
@@ -23,7 +23,6 @@ export default function CalendarView() {
       };
     });
   }, [data]);
-
 
   const handleSelectSlot = (slotInfo) => {
     const clickedDate = moment(slotInfo.start).format("DD-MM-YYYY");
@@ -42,6 +41,49 @@ export default function CalendarView() {
         onSelectSlot={handleSelectSlot}
         onSelectEvent={handleSelectSlot}
       />
+
+      {alert && (
+        <div style={{ marginTop: "20px", padding: "10px", background: "#fff3cd", color: "#856404", border: "1px solid #ffeeba" }}>
+         {alert}
+        </div>
+      )}
+
+      {selectedData && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0,0,0,0.5)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div style={{ background: "white", padding: "20px", borderRadius: "8px", width: "600px" }}>
+            <h3>Data for {selectedDate}</h3>
+
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={selectedData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="user" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="value" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+
+            <button
+              onClick={() => dispatch(clearSelection())}
+              style={{ marginTop: "15px", padding: "8px 16px", background: "#007bff", color: "white", border: "none", borderRadius: "4px", cursor: "pointer" }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
